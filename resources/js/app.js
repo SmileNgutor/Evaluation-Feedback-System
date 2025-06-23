71ffb594a068875efb33a0ad32e54ce89b56963d
mahmud <mahmudmodibbo21@gmail.com>
2025-06-24 00:35:24 +0100

    pre line seems to be working

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import 'preline';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     window.HSStaticMethods.autoInit(); // Add this
+// });
+// Run after DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    initComponents(); // 👈 initialize Preline
+});
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
